1f38f16cab9fe89592039f3681682a713fae5046
MdanzDev <nightmaremoon297@gmail.com>
2026-08-01 16:46:39 +0800

    x

--- src/lib/kryznet.ts
+++ src/lib/kryznet.ts
@@ -2,13 +2,13 @@ export const getKryzNetApiUrl = () =>
   process.env.EXTERNAL_API_URL || "https://api.kryz-net.space";
 
 export const getKryzNetApiKey = () =>
-  process.env.EXTERNAL_API_KEY || "";
+  process.env.EXTERNAL_API_KEY || "kryz_live_c20fabc004eed526bd2b924ee38ab3c861f3ff32";
 
 export const getBotSecret = () =>
   process.env.BOT_SECRET || "nickstore_secret_bot_key_2026";
 
 export const getAdminSecret = () =>
-  process.env.ADMIN_SECRET || "";
+  process.env.ADMIN_SECRET || "admin_secret_key_2026";
 
 export async function fetchKryzNetV2<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
   const apiKey = getKryzNetApiKey();
